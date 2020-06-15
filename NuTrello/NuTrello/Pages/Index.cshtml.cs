088643bfb7f80a97ab86b6066743424482910efc
Johkie <johannes_hp@hotmail.com>
2020-06-15 16:40:24 +0200

    Testing db support in index page

--- NuTrello/NuTrello/Pages/Index.cshtml.cs
+++ NuTrello/NuTrello/Pages/Index.cshtml.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
+using NuTrello.Models;
+using NuTrello.Data.Repository;
+
 namespace NuTrello.Pages
 {
     public class IndexModel : PageModel
@@ -16,9 +19,12 @@ namespace NuTrello.Pages
         // skall ta in alt. generera ett id som vi sedan appendar till boards ex. boards/{id}
 
         //random variable to vizualise an existing board
-        public List<string> boards =  new List<string>{"board one", "board two", "board three", "board four", "board five", "board six"};
-        private readonly ILogger<IndexModel> _logger;
+        //public List<string> boards =  new List<string>{"board one", "board two", "board three", "board four", "board five", "board six"};
+        public List<DbBoardModel> boards;
 
+        private readonly IBoardRepository _boardRepo;
+        private readonly IListRepository _listRepo;
+        private readonly ITaskRepository _taskRepo;
 
         public string insertList()
         {
@@ -26,16 +32,16 @@ namespace NuTrello.Pages
 
         }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(IBoardRepository boardRepository, IListRepository listRepository, ITaskRepository taskRepository)
         {
-            _logger = logger;
+            _boardRepo = boardRepository;
+            _listRepo = listRepository;
+            _taskRepo = taskRepository;
         }
 
         public void OnGet()
         {
-
-        }
-
-
+            boards = _boardRepo.GetBoards();
+        }   
     }
 }
